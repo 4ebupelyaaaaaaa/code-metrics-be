b2b83567fa0ced7e1cb8510c80672333a1bf08c2
Kristina Krupinina <Krupinina.KA@parmalogica.ru>
2025-06-24 19:07:58 +0500

    правка моделей

--- models/index.js
+++ models/index.js
@@ -1,19 +1,27 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const dbConfig = require("../config/db.config");
-const sequelize = new Sequelize(dbConfig.url, {
-  dialect: dbConfig.dialect,
-  dialectOptions: dbConfig.dialectOptions,
-});
 
+// Инициализируем Sequelize с параметрами из конфига
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    dialect: dbConfig.dialect,
+  }
+);
+
+// Используем фабрику модели User и AnalysisHistory
+const { User, AnalysisHistory } = require("./user.model")(sequelize, DataTypes);
+
+// Собираем объект db
 const db = {
   Sequelize,
   sequelize,
+  User,
+  AnalysisHistory,
 };
-
-// Подключаем нашу фабрику — она вернёт обе модели
-const { User, AnalysisHistory } = require("./user.model")(sequelize, DataTypes);
-
-db.User = User;
-db.AnalysisHistory = AnalysisHistory;
 
 module.exports = db;
